80ec67196645c7d260d72b45a5de21e13b666e29
Marcin Bieniek <marcingim@gmail.com>
2022-08-04 22:48:58 +0200

    Add search dropdown menu structure

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -10,9 +10,28 @@ const ProductSearch = () => (
   <form action='' className={styles.root}>
     <div className={styles.category}>
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
+
+      <ul id='dropmenu'>
+        <li>Select category</li>
+        <ul>
+          <li>
+            <a href='#'>Furniture</a>
+          </li>
+          <li>
+            <a href='#'>Chair</a>
+          </li>
+          <li>
+            <a href='#'>Table</a>
+          </li>
+          <li>
+            <a href='#'>Sofa</a>
+          </li>
+          <li>
+            <a href='#'>Bedroom</a>
+          </li>
+        </ul>
+      </ul>
+
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
