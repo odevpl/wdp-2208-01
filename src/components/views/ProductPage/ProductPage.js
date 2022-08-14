bff56a27794c06c96508c937e3956a13ddbe7870
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2022-08-14 12:39:14 +0200

    add section NewFurniture to ProductPage

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
-
+const ProductPage = () => {
+  return (
+    <div className={styles.root}>
+      <NewFurniture num={4} showNav={false} />
+    </div>
+  );
+};
 // ProductPage.propTypes = {};
 
 export default ProductPage;
